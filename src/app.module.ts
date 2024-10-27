5bb1bc7d0f01d440d2f746b3144b788096f1bfb0
cg <congvc98@gmail.com>
2024-10-27 17:05:21 +0700

    connecting to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [CommonModule, UsersModule],
+  imports: [CommonModule, UsersModule, DatabaseModule],
 })
 export class AppModule {}
