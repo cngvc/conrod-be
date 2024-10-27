121be4a292c2f3e10d1a0d308d72564646dcf279
cg <congvc98@gmail.com>
2024-10-27 16:41:22 +0700

    add validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { CommonModule } from './common/common.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [CommonModule, UsersModule],
 })
 export class AppModule {}
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,17 @@
+import { IsEmail, IsPhoneNumber, Length, Matches } from 'class-validator';
+
 export class CreateUserDto {
+  @Length(2, 50)
   readonly name: string;
+
+  @IsEmail()
   readonly email: string;
+
+  @IsPhoneNumber()
   readonly phone: string;
+
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Your password is too weak',
+  })
   readonly password: string;
 }
