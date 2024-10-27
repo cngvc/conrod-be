a3b7d5060d3e6df8136c815ad037bf31ca895ec2
cg <congvc98@gmail.com>
2024-10-27 17:11:11 +0700

    update user entity

---
+++ src/common/embedded/registry-dates.embedded.ts
@@ -0,0 +1,9 @@
+import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+export class RegistryDate {
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
