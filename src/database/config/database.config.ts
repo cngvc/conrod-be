906f9f91944a4c1c94b035e6544c13aa86ba46b6
cg <congvc98@gmail.com>
2024-10-28 07:52:47 +0700

    reduce dynamic module config

--- src/database/config/database.config.ts
+++ src/database/config/database.config.ts
@@ -1,7 +1,11 @@
 import { registerAs } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export default registerAs('database', () => {
   return {
+    type: 'postgres',
     url: process.env.DATASOURCE_URL,
-  };
+    autoLoadEntities: true,
+    synchronize: true,
+  } as const satisfies TypeOrmModuleOptions;
 });
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,24 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigType } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule.forFeature(databaseConfig)],
-      inject: [databaseConfig.KEY],
-      useFactory: (
-        databaseConfiguration: ConfigType<typeof databaseConfig>,
-      ) => {
-        return {
-          type: 'postgres',
-          url: databaseConfiguration.url,
-          autoLoadEntities: true,
-          synchronize: true,
-        };
-      },
-    }),
-  ],
+  imports: [TypeOrmModule.forRootAsync(databaseConfig.asProvider())],
 })
 export class DatabaseModule {}
