82dff8eba3e0315ede089354949a5419aa616499
cg <congvc98@gmail.com>
2024-10-29 15:43:29 +0700

    implement jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,25 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'users/entities/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import jwtConfig from './config/jwt.config';
 import { BcryptService } from './hashing/bcrypt.service';
 import { HashingService } from './hashing/hashing.service';
+import { LoginValidationMiddleware } from './middleware/login-validation/login-validation.middleware';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), PassportModule],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule,
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig),
+  ],
   controllers: [AuthController],
   providers: [
     AuthService,
@@ -17,8 +27,13 @@ import { LocalStrategy } from './strategies/local.strategy';
       provide: HashingService,
       useClass: BcryptService,
     },
+    JwtStrategy,
     LocalStrategy,
   ],
   exports: [HashingService],
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoginValidationMiddleware).forRoutes('auth/login');
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from 'users/entities/user.entity';
 import { HashingService } from './hashing/hashing.service';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
 import { RequestUser } from './interfaces/request-user.interface';
 
 @Injectable()
@@ -11,6 +13,7 @@ export class AuthService {
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
     private readonly hashingService: HashingService,
+    private readonly jwtService: JwtService,
   ) {}
 
   async validateLocal(email: string, password: string) {
@@ -34,5 +37,26 @@ export class AuthService {
       id: user.id,
     };
     return requestUser;
+  }
+
+  async validateJwt(payload: JwtPayload) {
+    const user = await this.usersRepository.findOneBy({ id: payload.sub });
+    if (!user) {
+      throw new UnauthorizedException('Invalid token');
+    }
+    const requestUser: RequestUser = {
+      id: payload.sub,
+    };
+    return requestUser;
+  }
+
+  login(user: RequestUser) {
+    const payload: JwtPayload = { sub: user.id };
+    return this.jwtService.sign(payload);
+  }
+
+  async getCurrentUserProfile(id: number) {
+    const user = await this.usersRepository.findOneBy({ id });
+    return user;
   }
 }
