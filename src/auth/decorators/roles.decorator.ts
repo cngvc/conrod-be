247b7ae91259d914fb10d637fcc21c966aa42d3b
cg <congvc98@gmail.com>
2024-10-29 23:33:26 +0700

    forbidden empty array when using roles decorator

--- src/auth/decorators/roles.decorator.ts
+++ src/auth/decorators/roles.decorator.ts
@@ -1,6 +1,8 @@
 import { SetMetadata } from '@nestjs/common';
 import { Role } from 'auth/roles/enums/role.enum';
+import { NonEmptyArray } from 'common/util/array.util';
 
 export const ROLE_KEY = 'roles';
 
-export const Roles = (...roles: Role[]) => SetMetadata(ROLE_KEY, roles);
+export const Roles = (...roles: NonEmptyArray<Role>) =>
+  SetMetadata(ROLE_KEY, roles);
