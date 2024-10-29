abbe2d7e3bb7a4a41cfb2f471df96efd2c0654d9
cg <congvc98@gmail.com>
2024-10-29 17:48:29 +0700

    add role to user entity

--- src/domain/users/entities/user.entity.ts
+++ src/domain/users/entities/user.entity.ts
@@ -1,3 +1,4 @@
+import { Role } from 'auth/roles/enums/role.enum';
 import { Exclude } from 'class-transformer';
 import { IsPassword } from 'common/decorators/validators/is-password.decorator';
 import { RegistryDates } from 'common/embedded/registry-dates.embedded';
@@ -22,6 +23,14 @@ export class User {
   @Column()
   @IsPassword()
   password: string;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+    enumName: 'role_enum',
+    default: Role.USER,
+  })
+  role: Role;
 
   @Column(() => RegistryDates, { prefix: false })
   registryDates: RegistryDates;
